4ecdae56d0db0fa133a6f68692ea1cb77e8e98df
Risheb.s <rishebs123456@gmail.com>
2025-09-21 02:27:18 +0530

    7th commit

--- src/components/UIBento.jsx
+++ src/components/UIBento.jsx
@@ -33,7 +33,7 @@ const UIBento = () => {
           <div className="relative z-10 h-full flex flex-col justify-end p-4">
             <div>
               <h4 className="text-3xl font-bold text-white mb-2">Hazzle Free Hosting</h4>
-              <p className="text-gray-100 text-sm">User-friendly design</p>
+              <p className="text-gray-100 text-sm">Providing continuous deployments & integrations</p>
             </div>
             
           </div>
